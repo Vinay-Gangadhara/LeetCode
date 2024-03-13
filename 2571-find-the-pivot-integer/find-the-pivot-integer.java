03665aaffe48125b516bd9866d7de4813cab7b2d
Vinay-Gangadhara <160822836+Vinay-Gangadhara@users.noreply.github.com>
2024-03-12 21:59:23 -0400

    Time: 0 ms (100.00%) | Memory: 40.9 MB (27.98%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,27 +1,22 @@
 class Solution {
     public int pivotInteger(int n) {
-        if(n == 1)
-        {
-            return 1;
-        }
-        if (n==2)
-        {
-            return -1;
-        }
-        else
-        {
+        
+        //Using Binary search for iteratinf to the list of Numbers
             int low = 1;
             int high = n;
             
             while(low<=high)
             {
                 int mid = (low+high)/2;
+                // Finding left half sum
                 int ans1 = (mid * (mid+1))/2;
+                //Finding the rright half Sum
                 int ans2 = ((n * (n+1))/2) - ans1 + mid;
                 if(ans1 == ans2)
                 {
                     return mid;
                 }
+                // If left sum is less we move right 
                 if(ans1<ans2)
                 {
                     low = mid+1;
@@ -31,7 +26,6 @@ class Solution {
                     high = mid-1;
                 }
             }
-        }
         return -1;
     }
 }
