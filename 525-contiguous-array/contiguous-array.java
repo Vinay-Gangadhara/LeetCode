ce9eef8632f3e4c8571c2b3bb9aa308ebc0cf340
Vinay-Gangadhara <160822836+Vinay-Gangadhara@users.noreply.github.com>
2024-03-16 15:42:43 -0400

    Time: 23 ms (79.94%) | Memory: 54.8 MB (80.10%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -4,22 +4,25 @@ class Solution {
         int count =0;
         HashMap<Integer,Integer> hm = new HashMap<>();
         hm.put(0,-1);
-        
+        //Iterate through each element
         for(int i = 0;i<nums.length;i++)
         {
+            // if we get 1 increment the counter
             if(nums[i] == 1)
             {
                 count++;
             }
+            // If we get 0 decrement the counter
             else
             {
                 count--;
             }
-            
+            // we will check if the counter has any same value in hm, if yes find the max length
             if(hm.containsKey(count))
             {
                 max = Math.max(max,(i-hm.get(count)));
             }
+            // if no match add that element into the hm with the first occurance of the count.
             else
             {
                 hm.put(count,i);
