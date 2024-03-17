3d33b0f26c339321517dd20f5dc885fb24be04db
Vinay-Gangadhara <160822836+Vinay-Gangadhara@users.noreply.github.com>
2024-03-17 02:16:56 -0400

    Time: 2 ms (44.45%) | Memory: 44.6 MB (74.25%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,16 +1,19 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        
+        //Initialising the array
         int n= intervals.length;
         List<int[]> result = new ArrayList<>();
         int i=0;
-        
+        // check for each element
         while(i<n)
         {
+            // if last elemt of array > first elemet in new array then cannor merge
             if(intervals[i][1] < newInterval[0])
                 result.add(intervals[i]);
+            // if first element in array > last element in new array cannot merge
             else if(intervals[i][0] > newInterval[1])
                 break;
+            // if we can merge : [0] => min of both, [1] => max of both
             else
             {
                 newInterval[0] = Math.min(intervals[i][0],newInterval[0]);
@@ -18,7 +21,9 @@ class Solution {
             }
             i++;
         }
+        // add the new merged interval
         result.add(newInterval);
+        // after merging if any elements available add them directly
         while(i<n)
         {
             result.add(intervals[i]);
