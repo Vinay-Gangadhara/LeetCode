50206f4a7f1afdbe2b0321fc0569b53b4e74fdb1
Vinay-Gangadhara <160822836+Vinay-Gangadhara@users.noreply.github.com>
2024-03-17 03:39:42 -0400

    Time: 0 ms (100.00%) | Memory: 42.7 MB (49.45%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,5 +1,8 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
+        
+        // Finding all the subsets for a given array
+        // Initialising the data containers
         List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
         List<Integer> ds = new ArrayList<>();
@@ -9,15 +12,17 @@ class Solution {
     
     public void funcs(int indx, int[] nums, int n, List<List<Integer>> ans, List<Integer> ds)
     {
+        // if index == n then we have reached the end need to return the elements stored in ds
         if(indx == n)
         {
             ans.add(new ArrayList<>(ds));
             return;
         }
-        
+        // take condition 
         ds.add(nums[indx]);
         funcs(indx+1,nums,n,ans,ds);
         ds.remove(ds.size()-1);
+        // not take condition
         funcs(indx+1,nums,n,ans,ds);
     }
 }
