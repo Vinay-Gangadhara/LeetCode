5aa2876eb2cfc68b090009b559547a1214a50c78
Vinay-Gangadhara <160822836+Vinay-Gangadhara@users.noreply.github.com>
2024-03-18 00:48:08 -0400

    Time: 52 ms (33.36%) | Memory: 73.8 MB (27.77%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -2,17 +2,21 @@ class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         
         int count = 0;
-        
+        // sort the array in acending order for start element
         Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
         int last = intervals[0][1];
-        
+        // iterate for 2nd element to last
         for(int i=1; i<intervals.length;i++)
         {
+            // if prev last > curr starting then overlapp
+            // increment counter +1
+            // find min of lasts values among both, bcz we need to remove min no. of ballons. if largerange baloon is present it will effect the calculation
             if(last > intervals[i][0])
             {
                 count++;
                 last = Math.min(last,intervals[i][1]);
             }
+            // else take the last of next array
             else
             {
                 last = intervals[i][1];
