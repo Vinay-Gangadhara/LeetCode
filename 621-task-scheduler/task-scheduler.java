342ef29c8c65839ae14ac0fbfedaabcae9c35255
Vinay-Gangadhara <160822836+Vinay-Gangadhara@users.noreply.github.com>
2024-03-19 03:03:00 -0400

    Time: 18 ms (53.33%) | Memory: 46.4 MB (5.12%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -2,7 +2,7 @@ class Solution {
     public int leastInterval(char[] tasks, int n) {
         
         HashMap <Character, Integer> hm = new HashMap<>();
-        
+        // iterate to the array find the char counts
         for(char i : tasks)
         {
             if(hm.containsKey(i))
@@ -16,7 +16,10 @@ class Solution {
         }
         List<Integer> lst = new ArrayList<>(hm.values());
         Collections.sort(lst , Collections.reverseOrder());
-        
+        // sort the array
+        //take the max element * n to minimise the ideal task count
+        //keep adding the rest of the elements such that they  fit in between the spaces or after the max element * cycles
+        //now we have idea spaces available add the total number of element in the array to get the iterations/intervals
         int max_freq = lst.get(0)-1;
         int spaces = max_freq * n;
         for(int i=1; i<lst.size();i++)
