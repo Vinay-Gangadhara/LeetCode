320766d1f0d38824d2794b73d8d387c3e8c9c972
Vinay Gangadhara <vinaygangadhar02@gmail.com>
2024-06-20 07:44:40 -0400

    Time: 43 ms (58.58%) | Memory: 55.9 MB (37.12%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.java
@@ -4,36 +4,45 @@ class Solution {
         int low = 1;
         int high = position[position.length-1];
         int ans = -1;
+        // Finding my answer range (min,max)
         while(low<=high)
         {
             int mid = (low+high)/2;
+            // Applying BS on Answers
             if(calculate(mid,position,m))
             {
+                // If calculation is true that means distance is less we need to incease the range
                 ans = mid;
                 low = mid+1;
             }
             else
             {
+                // Else condition means the distance is very lasrge we cannot accomodate all bals so need to reduce the distance
                 high = mid-1;
             }
         }
-        return ans;
+        // returning the pivot value;
+        return high;
     }
 
     public boolean calculate(int mid,int[] position, int m)
     {
+        //Initially placing 1 ball in first basket and incrementing the ball count
         int prevposition = 0; 
         int ballsplaced = 1;
 
         for(int i=1; i<position.length;i++)
         {
-            //int current_position = prevposition+mid;
+            //Checking the distance between prev placed ball and current basket.
+            // If distance is greater we increment the ball count and change the prev placed ball to current.
             if(position[i] - position[prevposition]>=mid)
             {
                 prevposition = i;
                 ballsplaced++;
             }
         }
+        // Now check the total ball placed for distance mid.
+        // If No.of Balls placed > m return true else False 
         if(ballsplaced >= m)
         {
             return true;
