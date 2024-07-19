aa8f00783a404480be11ca75e406bd78e15b6304
Vinay Gangadhara <vinaygangadhar02@gmail.com>
2024-07-19 07:05:27 -0400

    Time: 10 ms (55.50%) | Memory: 45 MB (19.83%) - LeetSync

--- 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.java
+++ 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.java
@@ -20,20 +20,30 @@ class Solution {
         return noOfPairs;
     }
 
+    //Have a BFS method to find the result
+    //First go to the leaf node once we go to parent keep adding 1 
+    // Compare the left list with right list
+    // Count No.of pairs possible
+    // Then only sent those units which is less than d-1 in new List to the parent
+    // return the No. of Pairs. 
     public ArrayList<Integer> dfs(TreeNode root, int dist)
     {
+        // If the root value is null return null
         if(root == null)
         {
             return new ArrayList<>();
         }
+        // If the left and right is null then its leaf node
+        // then return list with 1 
         if(root.left == null && root.right==null)
         {
             return new ArrayList<>(Arrays.asList(1));
         }
-
+        // iterate to the left of tree then right
         ArrayList<Integer> leftv = dfs(root.left,dist);
         ArrayList<Integer> rightv = dfs(root.right,dist);
 
+        // Compare left and right list and find number of pairs possible
         for(Integer lv : leftv)
         {
             for(Integer rv : rightv)
@@ -44,8 +54,9 @@ class Solution {
                 }
             }
         }
-
+        // Create a new list 
         ArrayList<Integer> parent = new ArrayList<>();
+        // Iterate to the left list and increment all the value which is less than d-1 and to parent list and sent to parent
         for(Integer lv : leftv)
         {
             if(lv+1 <= dist)
@@ -53,7 +64,7 @@ class Solution {
                 parent.add(lv+1);
             }
         }
-
+        // Iterate to the right and increment the value and add thopse to new parent list
         for(Integer rv : rightv)
         {
             if(rv+1 <= dist)
