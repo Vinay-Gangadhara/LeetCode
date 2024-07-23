406887be978e74bfd37685121e2c69d1c7fd581e
Vinay Gangadhara <vinaygangadhar02@gmail.com>
2024-07-23 19:18:02 -0400

    Time: 6 ms (97.98%) | Memory: 47.6 MB (61.77%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,5 +1,12 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+
+        /**
+        -------> Main Idea
+        1. Create an map that stores least possible value of a word(Asc order) and values the words when sorted equals to the key.
+        2. Once we pair them we know the Values are all grouped in there anagrams.
+        3. just return the values as answer.
+         */
         HashMap<String, List<String>> mp = new HashMap<>();
         for(String str : strs)
         {
