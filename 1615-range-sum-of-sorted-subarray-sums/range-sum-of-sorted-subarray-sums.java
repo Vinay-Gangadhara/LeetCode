94bedb6996abaccf35b20f6531b83a9ab3e54237
Vinay Gangadhara <vinaygangadhar02@gmail.com>
2024-08-04 19:47:28 -0400

    Time: 65 ms (70.08%) | Memory: 44.5 MB (78.28%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -1,6 +1,6 @@
 class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
-    PriorityQueue<Pair> minHeap = new PriorityQueue<>();
+    PriorityQueue<Pair> minHeap = new PriorityQueue<>((p1,p2) -> Integer.compare(p1.first,p2.first));
     int M = 1000000007;    
     int count = 1;
     int result = 0;
@@ -28,7 +28,7 @@ class Solution {
     return result;
     }
 }
-  class Pair implements Comparable<Pair> {
+  class Pair {
         int first;
         int second;
 
@@ -36,9 +36,14 @@ class Solution {
             this.first = first;
             this.second = second;
         }
-
-        @Override
-        public int compareTo(Pair other) {
-            return Integer.compare(this.first, other.first);
-        }
     }
+
+    /*
+    -------> Main Idea:
+    1. Create a Min Heap which will hold a Pair(Sum,index).
+    2. Everytime you pop the items from Heap, you will keep getting increasing order Sum of Sub-Arrays.
+    3. New Keep a counter to only have result from left to right;
+    4. Once we Pop element add the next index value into sum and increment index. Make sure index < n before you add to sum.
+    5. Once have result in range left to right return result.
+    6. minHeap requires to implement comparable because priorityQueue only sorts by natural order for Custom compare need a Comparable.
+    */
