3e1d79227cc3089860c2af28e1e2135f61f7e23d
Vinay Gangadhara <vinaygangadhar02@gmail.com>
2024-08-08 22:41:27 -0400

    Time: 21 ms (28.70%) | Memory: 45.5 MB (73.72%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minimumPushes(String word) {
+        Integer sortedOrder[] = new Integer[26];
+        int result = 0;
+        for (int i = 0; i < sortedOrder.length; i++) {
+            sortedOrder[i] = 0;
+        }
+        for(int i=0;i<word.length();i++)
+        {
+            Integer value = word.charAt(i) - 'a';
+            sortedOrder[value] ++;
+        }
+        
+        Arrays.sort(sortedOrder, Collections.reverseOrder());
+
+        for(int i=0;i<sortedOrder.length;i++)
+        {
+            result += (i/8 + 1)*sortedOrder[i] ;
+        }
+
+        return result;
+    }
+
+
+
+    /**
+    
+    -------> Main Idea:
+    1. Use Greedy Algorithm
+    2. Sort the frequenct in descind order
+    3. Keep adding the character in 8 buttons. */
+}
